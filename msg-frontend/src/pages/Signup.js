331e36ae8d495ff6f1e6cc4b7cbc950021f62ec1
Aadarsh Nagrath <anagrath1@gmail.com>
2023-08-14 03:57:09 +0530

    Created signup, and uploading feature to cloudinary

--- msg-frontend/src/pages/Signup.js
+++ msg-frontend/src/pages/Signup.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { Container, Col, Row } from 'react-bootstrap';
@@ -6,17 +6,63 @@ import './Signup.css';
 import { Link } from 'react-router-dom';
 import BotImg from '../assets/avatar.jpg';
 
-const validateImg = () => {};
 
 function Signup() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [name, setName] = useState("");
+
+    //image upload states
+    const [image, setImage] = useState(null);
+    const [uploadingImg, setUploadingImg] = useState(false);
+    const [imagePreview, setImagePreview] = useState(null);
+
+    function validateImg(e) {
+        const file = e.target.files[0];
+        if (file.size >= 1048576) {
+            return alert("Max file size is 1mb");
+        } else {
+            setImage(file);
+            setImagePreview(URL.createObjectURL(file));
+        }
+    }
+
+    async function uploadImage() {
+        const data = new FormData();
+        data.append("file", image);
+        data.append("upload_preset", "qop1bzdh");
+        try {
+            setUploadingImg(true);
+            let res = await fetch("https://api.cloudinary.com/v1_1/dzskaun8s/image/upload", {
+                method: "post",
+                body: data,
+            });
+            const urlData = await res.json();
+            setUploadingImg(false);
+            return urlData.url;
+        } catch (error) {
+            setUploadingImg(false);
+            console.log(error);
+        }
+    }
+
+    async function handleSignup(e) {
+        e.preventDefault();
+        if (!image) return alert("Please upload your profile picture");
+        const url = await uploadImage(image);
+        console.log(url);
+        //Now signing up the user -
+
+    }
+
     return (
         <Container>
             <Row>
                 <Col md={7} className="d-flex align-items-center justify-content-center flex-direction-column">
-                    <Form style={{width: "80%" , maxwidth: 500}}>
+                    <Form style={{width: "80%" , maxwidth: 500}} onSubmit={handleSignup}>
                       <h1 className='text-center'>Create Account</h1>
                       <div className='signup-profile-pic__container'>
-                        <img src={BotImg} className='signup-profile-pic' alt="default-img"/>
+                        <img src={imagePreview || BotImg} className='signup-profile-pic' alt="default-img"/>
                         <label className='image-upload-label' htmlFor="image-upload">
                           <i className='fas fa-plus-circle add-picture-icon'></i>
                         </label>
@@ -25,15 +71,20 @@ function Signup() {
                       </div>
                     <Form.Group className="mb-3" controlId="formBasicName">
                         <Form.Label>Name</Form.Label>
-                        <Form.Control type="text" placeholder="Enter your Name!" />
+                        <Form.Control type="text" placeholder="Enter your Name!" onChange={(e)=> setName(e.target.value)} value={name} />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="text" placeholder="Enter your email here!" onChange={(e)=> setEmail(e.target.value)} value={email} />
                     </Form.Group>
                 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control type="password" placeholder="Password" onChange={(e)=> setPassword(e.target.value)} value={password} />
                     </Form.Group>
                     <Button variant="primary" type="submit">
-                        Create Account
+                        {uploadingImg? 'Signing You up ....' : 'Create Account'}
                     </Button>
                     <div className='py-4'>
                         <p className='text-center'>Already have an account? <Link to="/login">Login</Link> </p>
